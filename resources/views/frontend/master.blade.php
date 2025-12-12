1f8ab7eb775a1a31efbda3f701f5944dc9072abf
mohannad <mohannad.abbas.dev@gmail.com>
2025-12-12 23:09:35 +0300

    upload

---
+++ resources/views/frontend/master.blade.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+
+@include('frontend.partials.head')
+
+<body>
+    <div class="container-xxl bg-white p-0">
+        <!-- Spinner Start -->
+        <div id="spinner"
+            class="show bg-white position-fixed translate-middle w-100 vh-100 top-50 start-50 d-flex align-items-center justify-content-center">
+            <div class="spinner-grow text-primary" style="width: 3rem; height: 3rem;" role="status">
+                <span class="sr-only">Loading...</span>
+            </div>
+        </div>
+        <!-- Spinner End -->
+
+
+        <!-- Navbar & Hero Start -->
+        <div class="container-xxl position-relative p-0">
+            @include('frontend.partials.navbar')
+
+
+            @include('frontend.partials.header')
+
+        </div>
+        <!-- Navbar & Hero End -->
+
+        @yield('content')
+
+
+        @include('frontend.partials.footer')
+
+
+
+        <!-- Back to Top -->
+        <a href="#" class="btn btn-lg btn-secondary btn-lg-square back-to-top"><i
+                class="bi bi-arrow-up"></i></a>
+    </div>
+
+    @include('frontend.partials.script')
+</body>
+
+</html>
---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+//-------------------Frontend Routes-------------------//
+Route::prefix('/')->name("front.")->group(function () {
+    //=============================== INDEX Pages ===================//
+    Route::view('/', 'frontend.index')->name('index');
+    //=============================== ABOUT Pages ===================//
+    Route::view('/about', 'frontend.about')->name('about');
+    //=============================== CONTACT Pages ===================//
+    Route::view('/contact', 'frontend.contact')->name('contact');
+    //=============================== PROJECTS Pages ===================//
+    Route::view('/projects', 'frontend.projects')->name('projects');
+    //=============================== SERVICE Pages ===================//
+    Route::view('/service', 'frontend.service')->name('service');
+    //=============================== TEAM Pages ===================//
+    Route::view('/team', 'frontend.team')->name('team');
+    //=============================== TESTIMONIAL Pages ===================//
+    Route::view('/testimonial', 'frontend.testimonial')->name('testimonial');
+});
